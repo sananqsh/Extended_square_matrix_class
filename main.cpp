b451df5c93fe52ec1855f08cb5869306ab8896c2
SananNaghshbandi <sananqsh@gmail.com>
2021-09-07 15:16:56 +0430

    couldn`t fix the non-square problem for +, - and *; so i just move on to get over with other things

--- main.cpp
+++ main.cpp
@@ -30,67 +30,58 @@ public:
 	vector< vector<int> > get_elements() {return elements;}
 };
 
-//
-	Matrix::Matrix() //default constructor
-	{
-		row_no = ROW_NO;
-		col_no = COL_NO;
-		// elements = { {0,0} , {0,0} };
+Matrix::Matrix() //default constructor
+{
+	row_no = ROW_NO;
+	col_no = COL_NO;
+	vector<vector<int> > temp(row_no);
+	elements = temp;
 
-		// To prevent segmentation fault
-		vector<vector<int> > temp(row_no);
-		elements = temp;
-		
-		for (int i = 0; i < row_no; ++i)
+	for (int i = 0; i < row_no; ++i)
+	{
+		for (int i = 0; i < col_no; ++i)
 		{
-			for (int i = 0; i < col_no; ++i)
-			{
-				elements[i].push_back(0);
-			}
+			elements[i].push_back(0);
 		}
 	}
+}
 
-	Matrix::Matrix(int _row_no, int _col_no)
+Matrix::Matrix(int _row_no, int _col_no)
+{
+	row_no = _row_no;
+	col_no = _col_no;
+
+	// To prevent segmentation fault
+	vector<vector<int> > temp(row_no);
+	elements = temp;
+
+	for (int i = 0; i < row_no; ++i)
 	{
-		row_no = _row_no;
-		col_no = _col_no;
-
-		// To prevent segmentation fault
-		vector<vector<int> > temp(row_no);
-		elements = temp;
-
-		for (int i = 0; i < row_no; ++i)
+		for (int i = 0; i < col_no; ++i)
 		{
-			for (int i = 0; i < col_no; ++i)
-			{
-				elements[i].push_back(0);
-			}
+			elements[i].push_back(0);
 		}
 	}
+}
 
-	//we take the vector by reference for better performance
-	Matrix::Matrix(int _row_no, int _col_no, vector< vector<int> >& _elements)
-	{
-		row_no = _row_no;
-		col_no = _col_no;
-		elements = _elements;
-	}
+Matrix::Matrix(int _row_no, int _col_no, vector< vector<int> >& _elements)
+{
+	row_no = _row_no;
+	col_no = _col_no;
+	elements = _elements;
+}
 
-	void Matrix::set_element(int i, int j, int x) { elements[i][j] = x; }
+void Matrix::set_element(int i, int j, int x) { elements[i][j] = x; }
 
+//TODO: handle size issues with m
 	Matrix Matrix::operator+(const Matrix& m) const
 	{
-		//TODO: handle size issues with m
-		cerr << "tst1\n";
 		Matrix sum(row_no, col_no);
-		cerr << "tst0\n";
 		for (int i = 0; i < row_no; ++i)
 		{
-			cerr << "tsttt\n";
 			for (int j = 0; j < col_no; ++j)
 			{
 				sum.elements[i][j] = elements[i][j] + m.elements[i][j];
-				cout << sum.elements[i][j];
 			}
 		}
 
@@ -127,59 +118,54 @@ public:
 		Matrix product(row_no, col_no); // By default its elements are all 0
 		for (int i = 0; i < row_no; ++i)
 			for (int j = 0; j < col_no; ++j)
-			{
-				cout << elements[i][j] << endl;
 				product.elements[i][j] += elements[i][j] * a;
-			}
-
 
 		return product;
 	}
 
-	ostream& operator<<(ostream& os, Matrix& m)
-	{
-		vector< vector<int> >  elements = m.get_elements();
-		int row_no = m.get_row_no();
-		int col_no = m.get_col_no();
+ostream& operator<<(ostream& os, Matrix& m)
+{
+	vector< vector<int> >  elements = m.get_elements();
+	int row_no = m.get_row_no();
+	int col_no = m.get_col_no();
 
-		for (int i = 0; i < row_no; ++i)
+	for (int i = 0; i < row_no; ++i)
+	{
+		for (int j = 0; j < col_no; ++j)
 		{
-			for (int j = 0; j < col_no; ++j)
-			{
-				if (elements[i][j] < 10)
-					os << ' ';
-				os << elements[i][j] << ' ';
-			}
-			os << endl;
+			if (elements[i][j] < 10)
+				os << ' ';
+			os << elements[i][j] << ' ';
 		}
-		// cout << endl;
-		return os;
+		os << endl;
+	}
+	// cout << endl;
+	return os;
+}
+
+Matrix Matrix::transpose()
+{
+	if (row_no != col_no)
+	{
+		cout << "Matrix has to be a square matrix in order to have a transpose matrix";
+		exit(0);
 	}
 
-	Matrix Matrix::transpose()
+	vector<vector<int> > transpose_elements = elements;
+	for (int i = 0; i < row_no; ++i)
 	{
-		if (row_no != col_no)
+		for (int j = i + 1; j < col_no; ++j)
 		{
-			cout << "Matrix has to be a square matrix in order to have a transpose matrix";
-			exit(0);
+			int temp = transpose_elements[i][j];
+			transpose_elements[i][j] = transpose_elements[j][i];
+			transpose_elements[j][i] = temp;
 		}
-
-		vector<vector<int> > transpose_elements = elements;
-		for (int i = 0; i < row_no; ++i)
-		{
-			for (int j = i + 1; j < col_no; ++j)
-			{
-				int temp = transpose_elements[i][j];
-				transpose_elements[i][j] = transpose_elements[j][i];
-				transpose_elements[j][i] = temp;
-			}
-		}
-		return Matrix(row_no, col_no, transpose_elements);
 	}
+	return Matrix(row_no, col_no, transpose_elements);
+}
 
-
-	void input_elements(int r, int c, vector< vector<int> >& v);
-	Matrix build_matrix(int row_no, int col_no);
+void input_elements(int r, int c, vector< vector<int> >& v);
+Matrix build_matrix(int row_no, int col_no);
 
 int main() 
 {
@@ -193,27 +179,25 @@ int main()
 		cout << "Size of matrix2:\n";	
 		cin >> row >> col;
 		Matrix b = build_matrix(row, col);
-		// Matrix answer1(row, col), answer2(row, col);//, answer3(row, col);
-
-		// cerr << "trst\n";
+		//answer1(row, col), answer2(row, col);//, answer3(row, col);
 
 		Matrix answer1 = a + b;
 		Matrix answer2 = a - b;
-		// answer3 = a * b;
+		Matrix answer3 = a * b;
 
-		cout << answer1 << endl << answer2 << endl;// << answer3 << endl;
+		cout << answer1 << endl << answer2 << endl << answer3 << endl;
 
 		// Matrix transpose(row, col);
-		// transpose_a = a.transpose();
-		// transpose_b = b.transpose();
+		Matrix transpose_a = a.transpose();
+		Matrix transpose_b = b.transpose();
 
-		// cout << transpose_a << endl << transpose_b;
+		cout << transpose_a << endl << transpose_b;
 
-	// int multiplier;
-	// cout << "Enter a multiplier: ";
-	// cin >> multiplier;
-	// Matrix product = a * multiplier;
-	// cout << product;
+	int multiplier;
+	cout << "Enter a multiplier: ";
+	cin >> multiplier;
+	Matrix product = a * multiplier;
+	cout << product;
 
 	return 0;
 }
